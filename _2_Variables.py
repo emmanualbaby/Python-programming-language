d8e8f069380e74e53c9be8ca796cdbaad90485e0
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-10-14 15:26:45 +0530

    Add files via upload

---
+++ _2_Variables.py
@@ -0,0 +1,51 @@
+# Varibles 
+# Variables are containers for storing data values.
+
+x = 10  # x - Varible , 10 - Value 
+print(x)
+
+a = 'Emmanual'
+print(a)
+
+# Casting of variables
+
+print(type(x))
+print(type(a))
+
+x = str(x)
+print(x)
+print('x:',type(x)) 
+
+# Creating a string with different methods
+b = "Python"    # double quotes
+print(b)
+
+c = '''Python Programming'''
+print(c)
+
+d = """Machine Learning"""
+print(d)
+
+# Case sensitive in python
+a = 9
+A = 8
+print(a)
+
+
+first_name,last_name = "Emmanual","Baby"
+print(first_name)
+
+
+
+q = 7
+Q = "Ikku"
+print(q,Q)
+print(type(q))
+q = str(q)
+print(q)
+print(type(q))
+_emmanual = "ikku"
+print(_emmanual)
+
+
+  
