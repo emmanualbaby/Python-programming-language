e516dbd13eb447115bf05b2e01d82117b5b3c739
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-10-17 16:46:33 +0530

    Add files via upload

---
+++ _8_Loops.py
@@ -0,0 +1,24 @@
+# Python Loops
+# For
+# While
+
+# For Loop
+
+print('Emmanual')
+print('Emmanual')
+print('Emmanual')
+print('Emmanual')
+print('Emmanual')
+for i in range(2,5):
+    print(i,'Emmanual')
+
+# print all the first 10 digits
+for i in range(1,11):
+    print(i,end=",")
+
+
+# While Loop
+i = 0   
+while i<=10:
+    print(i)
+    i=i+1 
