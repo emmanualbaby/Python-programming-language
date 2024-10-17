7908381b678572443d25efa4204782a842b7a137
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-10-17 16:46:19 +0530

    Add files via upload

---
+++ _7_Strings.py
@@ -0,0 +1,62 @@
+#Strings
+
+print("Emmanual")
+print('Emmanual')
+
+#Quotes Inside Quotes
+
+print("I am emmanual")
+print("He is called 'Ikku'")
+print('He is called "Ikku"')
+
+#Assign String to a Variable
+
+x = 'hello'
+print(x)
+
+#Multiline Strings
+
+a = """i am emmamual ikku Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. """
+print(a)
+
+#three single quotes
+
+q = '''i am emmamual ikku Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.'''
+print(q)
+
+#Strings are Arrays
+
+g = "Hello, World!"
+print(g[5])
+
+#Looping
+
+for v in "vava ":
+  print(v)
+
+#String Length
+
+o = "Hello, World!"
+print(len(o))
+
+#Check String
+
+txt = "python is a high level "
+print("high" in txt)
+
+# IF
+
+ikku = "python is a high level"
+if"python" in ikku:
+  print("yes")
+
+# NOT
+  
+vava = "i am working in kochi"
+print("ikku" not in vava)
+
+# NOT IF
+
+vavaq = "i am working in kochi"
+if "ikku" not in vavaq:
+    print("yes")
