bec5752517de3dcc1917f285c276294c547d40f9
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-10-22 16:15:47 +0530

    Add files via upload

---
+++ _11_while_Loop.py
@@ -0,0 +1,30 @@
+# Q.1
+
+# 1     
+# 1 2   
+# 1 2 3 
+# 1 2 3 4
+# 1 2 3 4 5
+
+
+i=1
+z = int(input("Enter Your Digit : "))
+while i<=z:
+    c=1
+    while c<=i:
+        print(c,end=" ")
+        c=c+1
+    print()
+    i=i+1
+
+
+# Q.2
+
+# 1 
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+
+
