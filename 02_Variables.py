e0c91905af548cea58f27405b175c258cbabca08
emmanualikku <menothemmanualbaby@gmail.com>
2024-12-29 11:22:41 +0530

    Add files via upload

---
+++ 02_Variables.py
@@ -0,0 +1,55 @@
+# # Varibles 
+# # Variables are containers for storing data values.
+
+# x = 10  # x - Varible , 10 - Value 
+# print(x)
+
+# a = 'Emmanual'
+# print(a)
+
+# # Casting of variables
+
+# print(type(x))
+# print(type(a))
+
+# x = str(x)
+# print(x)
+# print('x:',type(x)) 
+
+# # Creating a string with different methods
+# b = "Python"    # double quotes
+# print(b)
+
+# c = '''Python Programming'''
+# print(c)
+
+# d = """Machine Learning"""
+# print(d)
+
+# # Case sensitive in python
+# a = 9
+# A = 8
+# print(a)
+
+
+# first_name,last_name = "Emmanual","Baby"
+# print(first_name)
+
+
+# # todo
+
+# q = 7
+# Q = "Ikku"
+# print(q,Q)
+# print(type(q))
+# q = str(q)
+# print(q)
+# print(type(q))
+# _emmanual = "ikku"
+# print(_emmanual)
+
+
+text = "There's a cat" 
+print(text)
+
+  
