f75103455588a0090f61dfbb2304f5877abe0992
emmanualikku <menothemmanualbaby@gmail.com>
2024-12-30 10:53:27 +0530

    Add files via upload

---
+++ 06_Casting.py
@@ -0,0 +1,44 @@
+#Variable Type
+
+#Integers
+
+x = int(1) 
+print(x,type(x))
+
+y = int(2.8)
+print(y,type(y)) 
+
+z = int("3") 
+print(z,type(z))
+
+#Floats
+
+x = float(1)
+print(x,type(x))
+
+y = float(2.8)
+print(y,type(y))
+
+z = float("3")
+print(z,type(z))
+
+w = float("4.2")
+print(w,type(w))
+
+#Strings
+
+x = str("s1")
+print(x,type(x))
+
+y = str(2)
+print(y,type(y))
+
+z = str(3.0)
+print(z,type(z))
+
+
+
+
+
+
+
