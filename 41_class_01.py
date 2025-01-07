288dafdc6eb80a44011667268451dbc2c32cd2d0
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-01-07 08:35:32 +0530

    Add files via upload

---
+++ 41_class_01.py
@@ -0,0 +1,24 @@
+class my_class:
+    q = 7
+    def my_f(self):
+        print("i am emmanual")
+    def my_fu(self):
+        print("hello")
+
+obj = my_class()
+print(obj.q)
+obj.my_f()
+obj.my_fu()
+
+
+
+Qew = 5
+print(type(Qew))
+
+
+ds = "i ame"
+s = ds.upper()
+print(f"{s}")
+print(type(ds))
+
+
