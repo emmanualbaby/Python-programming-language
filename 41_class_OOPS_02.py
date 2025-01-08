530b967a78283b6c55a6df75cac71e11f95bdc9e
Emmanual Baby <menothemmanualbaby@gmail.com>
2025-01-08 11:15:31 +0530

    Add files via upload

---
+++ 41_class_OOPS_02.py
@@ -0,0 +1,16 @@
+
+class phone:
+    q = (input("Enter Your data : "))
+    def my_funation(self):
+        print(f"phone data")
+    def my_fun(self):
+        print(f"64 GB Rom and 12 GB Ram ")
+    def my_funa(self):
+        print(f"banded phone")
+
+
+obj = phone()
+obj.my_funation()
+obj.my_fun()
+obj.my_funa()
+print(type(obj.q))
