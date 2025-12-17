36701acdcd604b32a52db5659a8a5393bc022e2b
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2025-12-17 18:26:08 +0530

    01_Python_intro.py
    Removed extra blank lines in the script.

--- 01_Python_intro.py
+++ 01_Python_intro.py
@@ -2,10 +2,11 @@
 print("Hello World")
 print('Emmanuel')
 
-for i in range(10):      # iterating values till 10
-    print(i)
+for i in range(10):      # iterating values till 10 
+    print(i) 
 
  
  
+
 
 
